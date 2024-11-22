919634eb16b03c39d682770a2a3073d86e85f743
Luc Hardbarger <luc.hardbarger@nice.com>
2024-11-22 15:50:18 -0500

    Provide a standard interface that we can use in other programs.

---
+++ Deref/Programs/IBranchDatabaseQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Deref.Programs;
+
+public interface IBranchDatabaseQueryRequest : IRequest<string>
+{
+    FilterType BranchFilter { get; set; }
+    string? SearchTerm { get; set; }
+    bool IsExcludeTests { get; set; }
+    bool IsIncludeAll { get; set; }
+    bool IsIncludeOnlyRequired { get; set; }
+    bool IsIncludeOnlyNonRequired { get; set; }
+    bool IsRecursive { get; set; }
+}
