7da20c9b18dc5b98b2ff942935a7d21d9edd3e2d
Luc Hardbarger <luc.hardbarger@nice.com>
2024-12-31 14:14:09 -0500

    Allows each entity record to capture it's own errors per #41.

--- MonoUtils.Domain/Data/SchemaRecord.cs
+++ MonoUtils.Domain/Data/SchemaRecord.cs
@@ -1,3 +1,6 @@
 ﻿namespace MonoUtils.Domain.Data;
 
-public abstract record SchemaRecord(string Name, string Path, bool IsRequired, bool DoesExist);
+public abstract record SchemaRecord(string Name, string Path, bool IsRequired, bool DoesExist)
+{
+    public List<string> Errors { get; set; } = new();
+}
